62af392f58ef0f8063aac208239383c6e3344998
Ted Chen <ted@nephilagraphic.com>
2014-03-20 18:01:32 -0300

    Added serverspec for ensuring changes.

---
+++ test/integration/knockd/serverspec/default_spec.rb
@@ -0,0 +1,44 @@
+require 'serverspec'
+require 'spec_helper'
+
+include Serverspec::Helper::Exec
+
+# Ensure the packages are installed
+%w{knockd}.each do |p|
+  describe package(p) do
+    it { should be_installed }
+  end
+end
+
+describe service('knockd') do
+  it { should be_enabled }
+  it { should be_running }
+  it { should be_monitored_by('monit') }
+end
+
+
+# Check config readable, writable settings
+%w{/etc/knockd.conf /etc/default/knockd}.each do |f|
+  describe file(f) do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should_not be_writable.by('others') }
+    it { should_not be_executable.by('others') }
+  end
+end
+
+describe file('/etc/default/knockd') do
+  its(:content) { should match /START_KNOCKD\s*=\s*1/ }
+  its(:content) { should match /KNOCKD_OPTS\s*=\s*"-i eth1"/ }
+end
+
+
+# Check that the sequences were created
+describe file('/etc/knockd.conf') do
+  it { should_not be_readable.by('others') }
+  it { should contain '[ssh]' }
+  it { should contain '[http]' }
+  it { should contain 'sequence = 123,124,125' }
+  it { should contain 'sequence = 1123,1124,1125' }
+end
