a09fb52d7695933b722730cb8be1b6eb5844cb8a
Li-Te Chen <datacoda@gmail.com>
2016-12-15 09:54:48 -0400

    Locking version 0.2.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'datacoda@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures knockd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.0'
+version '0.2.1'
 source_url 'https://github.com/datacoda/chef-knockd'
 issues_url 'https://github.com/datacoda/chef-knockd/issues'
 
